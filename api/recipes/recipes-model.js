ed56065ee1ae65f8c85137bba61bd558cd70544b
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-01 16:39:30 -0400

    able to retrieve data, WIP

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,1 +1,15 @@
 // model functions here
+const db = require('../../data/db-config')
+
+function get() {
+    return db('recipes')
+}
+
+function getById(recipe_id) {
+    return db('recipes').where('recipe_id', recipe_id).first()
+}
+
+module.exports = {
+    get,
+    getById
+}
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,8 +1,33 @@
 const router = require("express").Router()
+const Recipe = require('./recipes-model')
 
 // endpoints here
-router.get('/', (req, res) => {
-    res.send("Hello from recipes router")
+router.get('/', async (req, res, next) => {
+    try {
+        const recipes = await Recipe.get()
+        if (!recipes) {
+            res.json([])
+        } else {
+            res.json(recipes)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const recipe = await Recipe.getById(req.params.id)
+        if (!recipe) {
+            res.status(404).json({
+                message: "recipe not found"
+            })
+        } else {
+            res.json(recipe)
+        }
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
