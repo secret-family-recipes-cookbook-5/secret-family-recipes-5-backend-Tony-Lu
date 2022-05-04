1552f63f607434382bd70d90cd7672072ff7f26c
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-04 14:59:56 -0400

    eliminated unneeded code

--- api/recipes/recipes-middleware.js
+++ api/recipes/recipes-middleware.js
@@ -23,48 +23,7 @@ function validateRecipe (req, res, next) {
     }
 }
 
-function validateIngredients (req, res, next) {
-    const newIngredient = req.body
-    if (
-        !newIngredient.ingredient_name || 
-        !newIngredient.recipe_id
-    ) {
-        res.status(400).json({
-            message: "ingredient name and recipe id required"
-        })
-    } else {
-        next()
-    }
-}
-
-function checkIngredientId (req, res, next) {
-    next()
-}
-
-function validateInstructions (req, res, next) {
-    const newInstruction = req.body
-    if (
-        !newInstruction.step_number || 
-        !newInstruction.step_instruction || 
-        !newInstruction.recipe_id
-    ) {
-        res.status(400).json({
-            message: "step number, step instruction, and recipe id are required"
-        })
-    } else {
-        next()
-    } 
-}
-
-function checkInstructionId (req, res, next) {
-    next()
-}
-
 module.exports = { 
     checkRecipeId,
-    validateRecipe,
-    validateIngredients,
-    checkIngredientId,
-    validateInstructions, 
-    checkInstructionId
+    validateRecipe
 }
