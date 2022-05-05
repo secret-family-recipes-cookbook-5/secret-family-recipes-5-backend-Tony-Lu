7ca66ede33335810ad665784629efbf54000754d
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-05 00:09:43 -0400

    eliminated unneeded endpoint

--- api/instructions/instructions-router.js
+++ api/instructions/instructions-router.js
@@ -5,10 +5,6 @@ const {
 } = require('./instructions-middleware')
 const Instruction = require('./instructions-model')
 
-router.get('/', (req, res, next) => {
-    res.send('Hello from instructions router')
-})
-
 router.get('/:id', checkInstructionId, (req, res, next) => {
     Instruction.getInstructionById(req.params.id)
         .then(instruction => {
