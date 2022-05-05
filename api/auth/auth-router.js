ed82fc0f57d064432a8c4c282d1a9a7dd89462d5
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-05 19:48:06 -0400

    able to login user

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -28,24 +28,35 @@ router.post('/register', validateRegistration, usernameTaken, (req, res, next) =
         .catch(next)
 })
 
-// router.post('/login', (req, res, next) => {
-//     const { username, password } = req.body
-//     User.findBy({ username })
-//         .first()
-//         .then(user => {
-//             if (user && bcrypt.compareSync(password, user.password)) {
-//                 const token = buildToken(user)
-//                 res.json({
-//                     message: `Welcome, ${user.username}`,
-//                     token
-//                 })
-//             } else {
-//                 res.status(401).json({
-//                     message: "invalid credentials"
-//                 })
-//             }
-//         })
-//         .catch(next)
-// })
+router.post('/login', (req, res, next) => {
+    const { username, password } = req.body
+    User.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = buildToken(user)
+                res.json({
+                    message: `Welcome, ${user.username}`,
+                    token
+                })
+            } else {
+                res.status(401).json({
+                    message: "invalid credentials"
+                })
+            }
+        })
+        .catch(next)
+})
+
+function buildToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router
