c63bc704b761f86b8d7f43a3b24802f0f133d582
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-05 00:38:27 -0400

    eliminated unneeded code

--- api/ingredients/ingredients-router.js
+++ api/ingredients/ingredients-router.js
@@ -21,8 +21,6 @@ router.post(
     '/',
     validateIngredient, 
     (req, res, next) => {
-        // res.send('add ingredients')
-        console.log("POST req.body ==>", req.body)
         Ingredient.addIngredient(req.body)
             .then(newIngredient => {
                 res.status(201).json(newIngredient)
@@ -36,11 +34,8 @@ router.put(
     validateIngredient,
     checkIngredientId,
     (req, res, next) => { 
-        // res.send('update ingredient')
-        console.log("PUT req.body ==>", req.body)
         Ingredient.updateIngredient(req.params.id, req.body)
             .then(updated => {
-                console.log("updated ==>", updated)
                 res.json(updated)
             })
             .catch(next)
@@ -51,19 +46,12 @@ router.delete(
     '/:id',
     checkIngredientId,
     async (req, res, next) => {
-        // res.send('delete ingredient')
         try {
-            console.log("DELETE req.params.id ==>", req.params.id)
             await Ingredient.deleteIngredient(req.params.id)
             res.json(req.ingredient)    
         } catch (err) {
             next()
         }
-        // .then(deleted => {
-            //     console.log("deleted ==>", deleted)
-            //     res.json(deleted)
-            // })
-            // .catch(next)
     }
 )
 
