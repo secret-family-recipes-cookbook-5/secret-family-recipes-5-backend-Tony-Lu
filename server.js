a32422dbdc93e50e2e6fde476b35a07727322e11
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-05-05 20:49:11 -0400

    path to use user router revised

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(cors())
 server.use(express.json())
 
 server.use('/api/auth', authRouter)
-server.use('/api/user', usersRouter)
+server.use('/api/users', usersRouter)
 server.use('/api/recipes', restricted, recipesRouter)
 server.use('/api/ingredients', restricted, ingredientsRouter)
 server.use('/api/instructions', restricted, instructionsRouter)
